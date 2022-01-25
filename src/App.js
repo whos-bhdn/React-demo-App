44d8b35fe5a6fb08da4b4a598947cb1277714c95
whos-bhdn <anonimoos13@gmail.com>
2022-01-25 22:09:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="simple">
+            <Technologies />
+            <Header />
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div className="simple">
+            <ul className="simpleColumn">
+                <li className="simpleListEl">Hello this is my first React App</li>
+                <li className="simpleListEl">Hello this is my first React App</li>
+                <li className="simpleListEl">Hello this is my first React App</li>
+                <li className="simpleListEl">Hello this is my first React App</li>
+                <li className="simpleListEl">Hello this is my first React App</li>
+            </ul>
+        </div>
+    );
+}
+
+const Technologies = () => {
+    return(
+        <ul className="simpleColumn">
+        <li className="simpleListEl">HTML</li>
+        <li className="simpleListEl">CSS</li>
+        <li className="simpleListEl">Bootstrap</li>
+        <li className="simpleListEl">Angular</li>
+        <li className="simpleListEl">Firebase</li>
+    </ul>
+    )
 }
 
 export default App;
